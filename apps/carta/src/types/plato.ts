79adbf30a9eec1c96728f97b458413f3583c1d1c
midas@air <javier.liano@alumnos.uneatlantico.es>
2026-07-13 19:57:15 +0200

    feat: Añadir ingredientes y medidas al type de plato

--- apps/carta/src/types/plato.ts
+++ apps/carta/src/types/plato.ts
@@ -1,6 +1,25 @@
 import type { Producto } from './producto';
 
+export interface Medida {
+  singular: string;
+  plural: string;
+}
+
+export interface Cantidad {
+  defecto: number;
+  minima: number;
+  maxima: number;
+  unidadMedida: Medida;
+}
+
+export interface Ingrediente {
+  nombre: string;
+  suplementoExtra: number;
+  cantidad: Cantidad;
+}
+
 export interface Plato extends Producto {
   descripcion: string;
   imagen: string;
+  ingredientes: Ingrediente[];
 }
